60202b3b873ed059137cda9d1b5e0639286a5a1f
zhanglei <1107604962@qq.com>
2021-01-29 23:07:55 +0800

    本地调通

--- train.py
+++ train.py
@@ -35,8 +35,8 @@ def get_args():
     parser = argparse.ArgumentParser('Yet Another EfficientDet Pytorch: SOTA object detection network - Zylo117')
     parser.add_argument('-p', '--project', type=str, default='visdrone2019', help='project file that contains parameters')
     parser.add_argument('-c', '--compound_coef', type=int, default=0, help='coefficients of efficientdet')
-    parser.add_argument('-n', '--num_workers', type=int, default=12, help='num_workers of dataloader')
-    parser.add_argument('--batch_size', type=int, default=12, help='The number of images per batch among all devices')
+    parser.add_argument('-n', '--num_workers', type=int, default=1, help='num_workers of dataloader')
+    parser.add_argument('--batch_size', type=int, default=1, help='The number of images per batch among all devices')
     parser.add_argument('--head_only', type=boolean_string, default=False,
                         help='whether finetunes only the regressor and the classifier, '
                              'useful in early stage convergence or small/easy dataset')
